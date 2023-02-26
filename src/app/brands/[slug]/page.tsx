c2fd71c33df9135c0fa46f230f942af76486ff9e
Rohan Bhangui <bhanguirohan@gmail.com>
2023-02-26 00:20:30 -0500

    fixed the seo

--- src/app/brands/[slug]/page.tsx
+++ src/app/brands/[slug]/page.tsx
@@ -15,10 +15,13 @@ export interface PageProps {
 export const generateMetadata = async ({ params }: { params: PageProps["params"]}) => {
   const data = await fetchData(params);
 
+  const title = `Spacd - ${data.title}`;
+  const seo_img = `${process.env.NEXT_PUBLIC_HOST}/img/${data.headerImage.split("/").at(-1)}`;
+
   return { 
-    title: `Spacd - ${data.title}`,
+    title: title,
     description: data.description,
-    applicationName: `Spacd - ${data.title}`,
+    applicationName: title,
     keywords: data.tags,
     icons: {
       icon: '/icon.png',
@@ -30,13 +33,13 @@ export const generateMetadata = async ({ params }: { params: PageProps["params"]
       },
     },
     openGraph: {
-      title: `Spacd - ${data.title}`,
+      title: title,
       description: data.description,
       url: `https://google.com`,
       siteName: 'Spacd',
       images: [
         {
-          url: `${process.env.NEXT_PUBLIC_HOST}/img/${data.headerImage.split("/").at(-1)}`,
+          url: seo_img,
         },
       ],
       locale: 'en-US',
@@ -44,9 +47,9 @@ export const generateMetadata = async ({ params }: { params: PageProps["params"]
     },
     twitter: {
       card: 'summary_large_image',
-      title: `Spacd - ${data.title}`,
+      title: title,
       description: data.description,
-      images: [`/img/${data.headerImage}`],
+      images: [seo_img],
     },
   }
 }
