99d443b7d008606603d029479c3c304b74bfe178
Rohan Bhangui <bhanguirohan@gmail.com>
2023-03-16 23:33:53 -0400

    Added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://projectspce.com',
+  generateRobotsTxt: true, // (optional)
+  generateIndexSitemap: false,
+  // ...other options
+}
+
+module.exports = config
